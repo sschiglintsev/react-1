767f5e85a7f66573a8b12bcceeecc0f68bb013f8
sockromant <s.s.chiglintsev@gmail.com>
2020-06-19 13:51:29 +0500

    388 lesson

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = (props) => {
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
                 <Route path='/settings' component={Settings}/>
+
             </div>
         </div>
   );
